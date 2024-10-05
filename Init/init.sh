3ab51020962e1928c1d661dd9fa3645ca12b5a0c
Visier <3146088404@qq.com>
2024-10-05 20:53:39 +0800

    version 0.0.0

--- Init/init.sh
+++ Init/init.sh
@@ -1,16 +1,19 @@
-DB_USER="root"
-DB_PASSWORD="root"
+read -p "DB_USER: " DB_USER
+read -p "DB_PASSWORD: " DB_PASSWORD
+
 SQL_FILE="./init.sql"
 
-mysql -u $DB_USER -p$DB_PASSWORD < $SQL_FILE
+mysql -u $DB_USER -p $DB_PASSWORD < $SQL_FILE
 # deal with the database
 
 
 # -------------------------------------------------
 FILE_PATH="../backend/app.js"
 SEARCH_TEXT="127.0.0.1"
-REPLACE_TEXT="0.0.0.0"
-sed -i "s/${SEARCH_TEXT}/${REPLACE_TEXT}/g" "${FILE_PATH}"
+
+read -p "DB_HOST: " DB_HOST
+
+sed -i "s/${SEARCH_TEXT}/${DB_HOST}/g" "${FILE_PATH}"
 
 # deal with the backend
 # -------------------------------------------------
@@ -26,6 +29,8 @@ sed -i "s/${SEARCH_TEXT}/${REPLACE_TEXT}/g" "${FILE_PATH}"
 # deal with the vite.config.js
 FILE_PATH="../frontend/src/http/index.js"
 SEARCH_TEXT="127.0.0.1"
+read -p "API_HOST: " API_HOST
 REPLACE_TEXT="42.121.125.52"
+sed -i "s/${SEARCH_TEXT}/${API_HOST}/g" "${FILE_PATH}"
 # deal with the frontend
 # -------------------------------------------------
--- Init/vblog.sql
+++ Init/vblog.sql
@@ -11,18 +11,18 @@
  Target Server Version : 80037 (8.0.37)
  File Encoding         : 65001
 
- Date: 03/10/2024 17:20:22
+ Date: 05/10/2024 20:38:59
 */
 
 SET NAMES utf8mb4;
 SET FOREIGN_KEY_CHECKS = 0;
--- ----------------------------
-DROP DATABASE IF EXISTS `vblog`;
-CREATE DATABASE IF NOT EXISTS `vblog` CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci;
-USE `vblog`;
+
 -- ----------------------------
 -- Table structure for blogbriefs
 -- ----------------------------
+CREATE DATABASE IF NOT EXISTS `vblog` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */; 
+USE `vblog`;
+
 DROP TABLE IF EXISTS `blogbriefs`;
 CREATE TABLE `blogbriefs`  (
   `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
@@ -34,11 +34,55 @@ CREATE TABLE `blogbriefs`  (
   `type` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `id` int NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`) USING BTREE
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of blogbriefs
 -- ----------------------------
+
+-- ----------------------------
+-- Table structure for blogs
+-- ----------------------------
+DROP TABLE IF EXISTS `blogs`;
+CREATE TABLE `blogs`  (
+  `blogUrl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of blogs
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for lifeslices
+-- ----------------------------
+DROP TABLE IF EXISTS `lifeslices`;
+CREATE TABLE `lifeslices`  (
+  `url` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of lifeslices
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for poetry
+-- ----------------------------
+DROP TABLE IF EXISTS `poetry`;
+CREATE TABLE `poetry`  (
+  `title` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `writer` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `recipient` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `content` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `date` datetime NULL DEFAULT NULL,
+  `manuscriptUrl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+
+-- ----------------------------
+-- Records of poetry
+-- ----------------------------
+INSERT INTO `poetry` VALUES (NULL, 'Visier', 'Soul', 'Welcome to world', NULL, NULL);
+INSERT INTO `poetry` VALUES (NULL, 'Soul', 'Visier', 'Fuck you world', NULL, NULL);
 
 -- ----------------------------
 -- Table structure for user
@@ -46,16 +90,16 @@ CREATE TABLE `blogbriefs`  (
 DROP TABLE IF EXISTS `user`;
 CREATE TABLE `user`  (
   `name` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `avatarurl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
-  `frameurl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `avatarUrl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
+  `frameUrl` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `brief` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `lover` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL,
   `like` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci NULL DEFAULT NULL
-) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = Dynamic;
+) ENGINE = InnoDB CHARACTER SET = utf8mb4 COLLATE = utf8mb4_0900_ai_ci ROW_FORMAT = DYNAMIC;
 
 -- ----------------------------
 -- Records of user
 -- ----------------------------
-INSERT INTO `user` VALUES ('Visier', NULL, NULL, 'Ah! I guess I have to write something here,\r\nin fact, I don\'t know what to write, so I just write something here.   Let me tell you a story...\r\n    of course not,asshole', 'X', 'X');
+INSERT INTO `user` VALUES ('Visier', 'https://avatars.akamai.steamstatic.com/7087fdfb89c501657a8e6e7854c2ada0e0852567_full.jpg', 'https://cdn.akamai.steamstatic.com/steamcommunity/public/images/items/253230/27ef3d62a0e42bf4ac60d426f2f172e1790ec0fd.png', 'Ah! I guess I have to write something here,\r\nin fact, I don\'t know what to write, so I just write something here.   Let me tell you a story...\r\n    of course not,asshole', 'X', 'X');
 
 SET FOREIGN_KEY_CHECKS = 1;
