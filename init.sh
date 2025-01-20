a2eff78b83fb760df1da7b6d6b7cc4d773fad50b
Visier <3146088404@qq.com>
2025-01-20 11:36:50 +0800

    supply

--- init.sh
+++ init.sh
@@ -28,12 +28,8 @@ sed -i "s#http://127.0.0.1:3000#${BASE_URL}#g" "${FILE_PATH}"
 # -------------------------------------------------
 FILE_PATH="frontend/custom.config.js"
 SEARCH_TEXT="http://127.0.0.1:3000"
-#read -p  "BACKEND_HOST: " BACKEND_HOST
 BACKEND_HOST="https://visier.icu/backend"
 sed -i "s#${SEARCH_TEXT}#${BACKEND_HOST}#g" "${FILE_PATH}"
-#SEARCH_TEXT="host: localhost"
-#read -p  "WEB_HOST: " WEB_HOST
-#sed -i "s/${SEARCH_TEXT}/host: '${WEB_HOST}',/g" "${FILE_PATH}"
 sed -i "s#open: true#open: false#g" "${FILE_PATH}"
 
 # deal with the frontend web host
@@ -49,15 +45,5 @@ npm i
 npm run build
 cd ../
 
-# deal with the dependency
-
-
-
-
 sudo systemctl daemon-reload
-
-# optional
-# sudo systemctl start vblog
-# sudo systemctl enable vblog
-# -------------------------------------------------
 
